c636f29465890fc42e9f6e280fd180034ae9a5fb
Hendrikto <hendrik.to@gmail.com>
2017-02-08 17:39:16 +0100

    Implement python server for 5b.

--- answers/server.py
+++ answers/server.py
@@ -1,4 +1,6 @@
+import os.path
 from socket import *
+
 serverPort = 20000
 # build TCP socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -7,20 +9,52 @@ serverSocket.bind(('', serverPort))
 # listen (clients will start queuing up)
 serverSocket.listen(1)
 
-print('The echo server is ready to receive')
-while 1:
-    # the is waiting for clients to connect/ processes them one at a time
-    connectionSocket, addr = serverSocket.accept()
-    print('Processing client ', addr)
-    try :
-        sentence = connectionSocket.recv(1024)
-        # while the other side hasn't closed the socket, do echo
-        while sentence:
-            echoedSentence = sentence
-            connectionSocket.send(echoedSentence)
-            sentence = connectionSocket.recv(1024)
+
+# author: Hendrik Werner s4549775
+
+
+def send_file(path):
+    if os.path.isfile(path):
+        connection.send(b"file_content\n")
+    else:
+        connection.send(b"file_not_found\n")
+
+def command_loop(connection):
+    while True:
+        received = connection.recv(1024).decode().strip()
+        if received == "bye":
+            connection.send(b"bye\n")
+            return
+        elif received.split()[0] == "get":
+            send_file(received.split()[1])
+        else:
+            connection.send(b"cannot_understand\n")
+
+
+def greeting(connection):
+    while True:
+        received = connection.recv(256).decode().strip()
+
+        if received == "hello":
+            connection.send(b"hello\n")
+            return
+        else:
+            connection.send(b"cannot_understand\n")
+
+
+def process_client(connection):
+    try:
+        greeting(connection)
+        command_loop(connection)
     except error:
-        pass
-    print('Client closed ', addr)
-    # close socket
-    connectionSocket.close()
+        print(error)
+
+
+if __name__ == "__main__":
+    print("Start the server.")
+    while True:
+        connection, address = serverSocket.accept()
+        print("Process client", address)
+        process_client(connection)
+        print("Close connection", address)
+        connection.close()
